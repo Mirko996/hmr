b0dc4842459a1f60cdbdeab4a29be5bcda7ff43c
Mirko Tomovic <mirko.tomovic@synergysuite.com>
2018-12-04 12:48:03 +0100

    Database finished

--- hrm_base.sql
+++ hrm_base.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2018 at 11:03 AM
+-- Generation Time: Dec 04, 2018 at 12:40 PM
 -- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -42,7 +42,21 @@ CREATE TABLE `branches` (
 --
 
 INSERT INTO `branches` (`id`, `address`, `city`, `email`, `name`, `password`) VALUES
-(1, 'Bulevar Dzordza Vasingtona 92', 'Podgorica', 'synergysuite@synergysuite.com', 'SynergySuite', 'synergysuite');
+(1, 'Bulevar Dzordza Vasingtona 92', 'Podgorica', 'synergysuite@synergysuite.com', 'SynergySuiteP1', 'synergysuite'),
+(2, 'Belvederska 22', 'Podgorica', 'exampleemail@gmail.com', 'SynergySuiteP2', 'belvederska123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shifts`
+--
+
+CREATE TABLE `shifts` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -65,7 +79,11 @@ CREATE TABLE `workers` (
 
 INSERT INTO `workers` (`id`, `fk_branc_id`, `name`, `last_name`, `email`, `password`) VALUES
 (1, 1, 'Anton', 'Djokaj', 'anton.djokaj@synergysuite.com', 'anton'),
-(2, 1, 'Mirko', 'Tomovic', 'mirko.tomovic@synergysuite.com', 'mirko');
+(2, 1, 'Mirko', 'Tomovic', 'mirko.tomovic@synergysuite.com', 'mirko'),
+(3, 1, 'Milan', 'Maric', 'milan.maric@gmail.com', 'milan123'),
+(4, 1, 'Andjela', 'Milic', 'aldjela.milic@gmail.com', 'andjela123'),
+(5, 1, 'Sava', 'Softic', 'sava123@gmail.com', 'sava123'),
+(6, 1, 'Milica', 'Milacic', 'milicam@gmail.com', 'milica123');
 
 --
 -- Indexes for dumped tables
@@ -76,6 +94,14 @@ INSERT INTO `workers` (`id`, `fk_branc_id`, `name`, `last_name`, `email`, `passw
 --
 ALTER TABLE `branches`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shifts`
+--
+ALTER TABLE `shifts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `worker_id` (`worker_id`),
+  ADD KEY `branch_id` (`branch_id`);
 
 --
 -- Indexes for table `workers`
@@ -92,17 +118,30 @@ ALTER TABLE `workers`
 -- AUTO_INCREMENT for table `branches`
 --
 ALTER TABLE `branches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `shifts`
+--
+ALTER TABLE `shifts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `workers`
 --
 ALTER TABLE `workers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `shifts`
+--
+ALTER TABLE `shifts`
+  ADD CONSTRAINT `shifts_ibfk_1` FOREIGN KEY (`worker_id`) REFERENCES `workers` (`id`),
+  ADD CONSTRAINT `shifts_ibfk_2` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`);
 
 --
 -- Constraints for table `workers`
